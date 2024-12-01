f5688e76a98be419ad39f165fcad4146c29bd24e
YuchenForeverJZ <jack.yc.zhang@outlook.com>
2024-11-30 18:00:46 -0800

    Latest changes to sign up and sign in with Clerk

--- homekey-frontend/src/App.js
+++ homekey-frontend/src/App.js
@@ -1,9 +1,63 @@
-import React from 'react'
+// App.js
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {
+  SignIn,
+  SignUp,
+  UserButton,
+  SignedIn,
+  SignedOut,
+  RedirectToSignIn,
+} from '@clerk/clerk-react';
 
-const App = () => {
+function App() {
   return (
-    <div>App home key</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/sign-in/*" element={<SignIn routing="path" path="/sign-in" />} />
+        <Route path="/sign-up/*" element={<SignUp routing="path" path="/sign-up" />} />
+        {/* Internal page, that is where the seller dashboard comes in */}
+        <Route path="/internal" element={<InternalPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
+
+// HomePage Component
+const HomePage = () => (
+  <div>
+    <h1>Welcome to the App</h1>
+    <SignedIn>
+      {/* If the user is signed in, show a link to the internal page */}
+      <p>
+        You are signed in. Go to the <a href="/internal">Internal Page</a>.
+      </p>
+    </SignedIn>
+    <SignedOut>
+      {/* If the user is signed out, prompt to sign in or sign up */}
+      <p>
+        Please <a href="/sign-in">Sign In</a> or <a href="/sign-up">Sign Up</a> to continue.
+      </p>
+    </SignedOut>
+  </div>
+);
+
+// InternalPage Component
+const InternalPage = () => (
+  <div>
+    <SignedIn>
+      {/* Content visible only to signed-in users */}
+      <h1>Internal Page</h1>
+      <UserButton />
+      <p>This is a protected page accessible only after signing in.</p>
+      {/* Add your internal content here */}
+    </SignedIn>
+    <SignedOut>
+      {/* Redirect unauthenticated users to the sign-in page */}
+      <RedirectToSignIn />
+    </SignedOut>
+  </div>
+);
--- homekey-frontend/src/index.js
+++ homekey-frontend/src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ClerkProvider } from '@clerk/clerk-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// const clerkPubKey = "pk_test_ZnVubnktZ2liYm9uLTQ1LmNsZXJrLmFjY291bnRzLmRldiQ";
+const clerkPubKey = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY
+console.log("process: ", process.env.REACT_APP_CLERK_PUBLISHABLE_KEY)
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ClerkProvider publishableKey={clerkPubKey}>
+      <App />
+    </ClerkProvider>
   </React.StrictMode>
 );
 
