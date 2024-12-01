854cd02b0e8991b77712300fb6c53d53a5ca9825
YuchenForeverJZ <jack.yc.zhang@outlook.com>
2024-11-30 18:01:48 -0800

    minor changes

--- homekey-frontend/src/index.js
+++ homekey-frontend/src/index.js
@@ -6,7 +6,6 @@ import reportWebVitals from './reportWebVitals';
 import { ClerkProvider } from '@clerk/clerk-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-// const clerkPubKey = "pk_test_ZnVubnktZ2liYm9uLTQ1LmNsZXJrLmFjY291bnRzLmRldiQ";
 const clerkPubKey = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY
 console.log("process: ", process.env.REACT_APP_CLERK_PUBLISHABLE_KEY)
 
@@ -17,8 +16,4 @@ root.render(
     </ClerkProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
